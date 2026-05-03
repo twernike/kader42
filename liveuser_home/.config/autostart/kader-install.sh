241792b2a757b1e06956481e5487e8453e5244df
twernike <tjochens1980@googlemail.com>
2026-05-03 21:58:13 +0200

    Add install button to welcome app

--- liveuser_home/.config/autostart/kader-install.sh
+++
@@ -1,3 +0,0 @@
-# sudo calamares
-mkdir -p install_log
-sudo calamares -d 2>&1 | tee install_log/calamares_debug.log
--- usr_data/bin/kader-install.sh
+++ usr_data/bin/kader-install.sh
@@ -1,26 +1,3 @@
-#!/bin/bash
-
-echo "************************************************************"
-echo " Kader⁴² Installer"
-echo "************************************************************"
-
-# We check whether ‘not found’ appears in the ldd output of Calamares. 
-# If it does, we will print the missing libraries and exit with an error message. 
-# If all libraries are found, we will start Calamares as usual.
-
-MISSING_LIBS=$(ldd /usr/bin/calamares | grep "not found")
-
-echo "[Kader⁴² Installer] Checking for missing libraries in Calamares..."
-if [ -z "$MISSING_LIBS" ]; then
-    echo "[Kader⁴² Installer]✅ All libraries loaded. Start Calamares..."
-else
-    echo "[Kader⁴² Installer]⛔ Missing libraries detected❗⚠️" 
-    echo "$MISSING_LIBS"
-    echo "[Kader⁴² Installer] Please try running ‘pacman -Syu’ in the live system.
-    If you are the administrator or user of this system, please ensure all required libraries are installed.
-    If all requirements are met, you can contact support@kader42.de. Thank you for your understanding."
-    exit 1
-fi
-
-# Start von Calamares
-sudo calamares #--config /etc/calamares/settings.conf -d
+# sudo calamares
+mkdir -p install_log
+sudo calamares -d 2>&1 | tee install_log/calamares_debug.log
